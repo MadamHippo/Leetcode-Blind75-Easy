2897e4e3c5dcaf2f337920b401eab98ac0769ace
Lily <88348716+MadamHippo@users.noreply.github.com>
2022-11-22 00:15:43 -0800

    Create 520-detect-capital.py

---
+++ 520-detect-capital.py
@@ -0,0 +1,29 @@
+/*
+We define the usage of capitals in a word to be right when one of the following cases holds:
+
+All letters in this word are capitals, like "USA".
+All letters in this word are not capitals, like "leetcode".
+Only the first letter in this word is capital, like "Google".
+Given a string word, return true if the usage of capitals in it is right.
+*/
+
+
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        
+        if len(word) <= 0:
+            return True
+        
+        elif word.upper() == word:
+            return True
+        
+        elif word.lower() == word:
+            return True
+        
+        elif word[0].upper() == word[0] and word.lower()[1:] == word[1:]:
+            return True
+        
