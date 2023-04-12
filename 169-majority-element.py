25cc72dc54c2107045c5972830a6207893d09e9e
Lily <88348716+MadamHippo@users.noreply.github.com>
2023-04-12 00:12:22 -0700

    Create 169-majority-element.py

---
+++ 169-majority-element.py
@@ -0,0 +1,17 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+
+# HASHMAP SOLUTION
+# runtime O(n) because the m is smaller (we know it is) than n so just go with n as the worst runtime
+
+        hashmap = {}
+                
+        for n in nums:
+            if n not in hashmap:
+                hashmap[n] = 1
+            else:
+                hashmap[n] += 1
+                
+        for key, value in hashmap.items():
+            if value > (len(nums)//2):
+                return key
