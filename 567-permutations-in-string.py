d7de25ca86b8aaf058024b8c52fe70a63a6f5f14
Lily <88348716+MadamHippo@users.noreply.github.com>
2023-04-18 00:12:47 -0700

    Update 567-permutations-in-string.py

--- 567-permutations-in-string.py
+++ 567-permutations-in-string.py
@@ -1,1 +1,22 @@
-TBA
+// notes needed
+// https://leetcode.com/problems/permutation-in-string/submissions/
+
+def checkInclusion(s1, s2):
+    if len(s1) > len(s2):
+        return False
+
+    s1_chars = [0] * 26
+    s2_chars = [0] * 26
+
+    for i in range(len(s1)):
+        s1_chars[ord(s1[i]) - ord('a')] += 1
+        s2_chars[ord(s2[i]) - ord('a')] += 1
+
+    for i in range(len(s1), len(s2)):
+        if s1_chars == s2_chars:
+            return True
+
+        s2_chars[ord(s2[i]) - ord('a')] += 1
+        s2_chars[ord(s2[i - len(s1)]) - ord('a')] -= 1
+
+    return s1_chars == s2_chars
